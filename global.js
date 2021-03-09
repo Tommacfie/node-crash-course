18ac5dafb9bcf9241634e272f3a9338caf1d4f54
Tommac <tommacfie@aol.com>
2021-03-09 18:07:13 +0100

    Intervals added

--- global.js
+++ global.js
@@ -1,1 +1,11 @@
+setTimeout(() => {
+  console.log('Timed Out!');
+  clearInterval(int);
+}, 3000);
+
+const int = setInterval(() => {
+  console.log('Timeout!')
+}, 1000);
+
+console.log(__dirname);
 console.log(__filename);
