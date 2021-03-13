900bca3097eaf12191433296f8fbb4b6640cee4c
Tommac <tommacfie@aol.com>
2021-03-13 15:01:16 +0100

    Updated server
    Signed-off-by: Tommac <tommacfie@aol.com>

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+  fs.readFile('./docs/blog2.txt', (err, data) => {
+    console.log(data.toString());
+  });
 
 })
 server.listen(3000, 'localhost', () => {
